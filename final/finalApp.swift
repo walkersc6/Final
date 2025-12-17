1684df791c307993da818db97e59b1600d4dd487
Sarah Walker <walkersc6>
2025-12-16 21:39:01 -0700

    Initial Commit

---
+++ final/finalApp.swift
@@ -0,0 +1,32 @@
+//
+//  finalApp.swift
+//  final
+//
+//  Created by Sarah Walker on 12/16/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct finalApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
